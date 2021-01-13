674936c7e01d09d9c5c7dc4979ec78be8fbb8789
eric.lwh <eric.lwh@alibaba-inc.com>
2021-01-13 14:53:52 +0800

    merge tvm to 2.x

--- packages/cli/src/bin/pipcook.ts
+++ packages/cli/src/bin/pipcook.ts
@@ -94,5 +94,9 @@ import devPlugin from '../actions/dev-plugin';
     .command('job', 'operate the job bound to specific pipeline')
     .command('pipeline', 'operate on pipeline');
 
+  program
+    .command('lib')
+    .description("add essential libs and deps for pipcook");
+
   program.parse(process.argv);
 })();
