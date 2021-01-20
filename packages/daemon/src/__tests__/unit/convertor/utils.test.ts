90ca5c1125419174ff345ff2b0a66e1f21fd496a
eric.lwh <eric.lwh@alibaba-inc.com>
2021-01-20 20:23:36 +0800

    add util tests

--- packages/daemon/src/__tests__/unit/convertor/utils.test.ts
+++ packages/daemon/src/__tests__/unit/convertor/utils.test.ts
@@ -17,3 +17,36 @@ test.serial('test initTVM', async (t) => {
 
   t.true(mockImport.calledThrice, 'import should be called thrice');
 });
+
+test.serial('test initTVM when tvm is not installed', async (t) => {
+
+  const mockImport = sinon.stub(boa, 'import');
+  mockImport.withArgs('tvm.relay').throws({});
+
+  t.throws(utils.initTVM);
+
+  t.true(mockImport.called, 'import should be called');
+});
+
+
+test.serial('test initTVM when emscripten is not installed', async (t) => {
+
+  const mockImport = sinon.stub(boa, 'import');
+  mockImport.withArgs('tvm.relay').returns({});
+  mockImport.throws({});
+  t.throws(utils.initTVM);
+
+  t.true(mockImport.called, 'import should be called');
+});
+
+test.serial('test initTVM when keras is not installed', async (t) => {
+
+  const mockImport = sinon.stub(boa, 'import');
+  mockImport.withArgs('tvm.relay').returns({});
+  mockImport.withArgs('tvm.contrib.emcc').returns({});
+  mockImport.throws({});
+
+  t.throws(utils.initTVM);
+
+  t.true(mockImport.called, 'import should be called');
+});
